c3c024e85fedcca243c8976e5b6d8bcf33be9507
John Anderson <sontek@gmail.com>
2015-02-05 09:58:18 -0800

    Added version file and release script

---
+++ release.sh
@@ -0,0 +1,14 @@
+# Before you release
+# - Bump version in the `VERSION` file
+# - update the package version in the CHANGES file/add any relevant changes
+# - send update to devops that new release is coming
+
+PKG_NAME=$(python setup.py --name)
+PKG_VERSION=$(cat VERSION)
+PKG_FILE=${PKG_NAME}-${PKG_VERSION}.tar.gz
+pip install setuptools-git
+git tag -am "release ${PKG_VERSION}" ${PKG_VERSION}
+git push --tags
+git checkout ${PKG_VERSION}
+python setup.py sdist upload
+git checkout ${CURRENT_BRANCH}
